bad1d7512ba0d382081f9769fb4687415f2232e0
Sherwood Richers <srichers@caltech.edu>
2021-07-15 17:14:09 -0700

    updated authentication. More descriptive and using personal access token instead of ssh key

--- jenkins/seedjob.groovy
+++ jenkins/seedjob.groovy
@@ -1,9 +1,9 @@
-multibranchPipelineJob('example') {
+multibranchPipelineJob('FLASH-X') {
     branchSources {
         git {
-            id('123456789') // IMPORTANT: use a constant and unique identifier
-            remote('git@github.com:ECP-Astro/FLASH5')
-            credentialsId('github-ssh')
+            id('1') // IMPORTANT: use a constant and unique identifier
+            remote('https://github.com/ECP-Astro/FLASH5')
+            credentialsId('github_personal_access_token')
             includes('master')
         }
     }
