5e73b2e9352a87699bac79d5a3ccc66f4703515e
Sherwood Richers <srichers@caltech.edu>
2021-07-15 17:37:34 -0700

    added two more seed jobs

--- jenkins/seedjob.groovy
+++ jenkins/seedjob.groovy
@@ -8,3 +8,25 @@ multibranchPipelineJob('FLASH-X') {
         }
     }
 }
+
+multibranchPipelineJob('BANG-neutrino-oscillation') {
+    branchSources {
+        git {
+            id('2') // IMPORTANT: use a constant and unique identifier
+            remote('https://github.com/snaphu-msu/BANG.git')
+            credentialsId('github_personal_access_token')
+            includes('nuMixingNotBroken')
+        }
+    }
+}
+
+multibranchPipelineJob('FLASH4-montecarlo') {
+    branchSources {
+        git {
+            id('3') // IMPORTANT: use a constant and unique identifier
+            remote('https://github.com/srichers/FLASH4.6.2.git')
+            credentialsId('github_personal_access_token')
+            includes('nuMixingNotBroken')
+        }
+    }
+}
