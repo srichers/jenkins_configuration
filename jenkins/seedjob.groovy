34747e4a7cff41e2d1b30a334af6997ff245c804
Sherwood Richers <srichers@caltech.edu>
2021-07-19 14:19:33 -0700

    reinstated flash-x job

--- jenkins/seedjob.groovy
+++ jenkins/seedjob.groovy
@@ -1,3 +1,14 @@
+multibranchPipelineJob('FLASH-X') {
+    branchSources {
+        git {
+            id('1') // IMPORTANT: use a constant and unique identifier
+            remote('https://github.com/ECP-Astro/FLASH5')
+            credentialsId('github_personal_access_token')
+            includes('master')
+        }
+    }
+}
+
 multibranchPipelineJob('BANG-neutrino-oscillation') {
     branchSources {
         git {
